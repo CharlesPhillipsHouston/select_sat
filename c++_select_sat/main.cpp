00e1ce420c04dfba487d3e8e475c26ac1982e343
Administrator <admin@phillips-mbp.attlocal.net>
2020-02-09 16:28:37 -0600

    Initial Commit

---
+++ c++_select_sat/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  c++_select_sat
+//
+//  Created by Administrator on 2/9/20.
+//  Copyright © 2020 charles. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
